81ff453105893c020a885511c99aa9098970ca27
aarnav007 <73699528+aarnav007@users.noreply.github.com>
2020-12-29 12:31:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var bullet,speed,weight;
+var thickness,wall;
+
+
+function setup() {
+  createCanvas(1600,400);
+  bullet = createSprite(300,100,10,30)
+  bullet.shapeColor="white"
+  
+
+}
+
+function draw() {
+  background(0);  
+  
+  drawSprites;
+}
